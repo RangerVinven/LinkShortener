135ea4e6c0f80e82b44a0fcb3ceaf7e8f6e332e1
Daniel McPherson <daniel.mcpherson@live.co.uk>
2023-11-23 18:42:31 +0000

    Added get users endpoint (development only)

--- api/routes/users.py
+++ api/routes/users.py
@@ -3,6 +3,11 @@ from models.User import CreateUser
 
 usersRouter = APIRouter()
 
+# For Development only
+@usersRouter.get("/")
+def getUsers():
+    return {}
+
 @usersRouter.post("/")
 def createUser(user: CreateUser):
     return user
