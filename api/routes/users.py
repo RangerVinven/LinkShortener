fc691f6df6b7b9cfb578c264fdf435784482702f
Daniel McPherson <daniel.mcpherson@live.co.uk>
2023-11-23 20:20:19 +0000

    Added SELECT query for the get users endpoint

--- api/routes/users.py
+++ api/routes/users.py
@@ -1,12 +1,15 @@
 from fastapi import APIRouter
+
 from models.User import CreateUser
+from database.database import cursor
 
 usersRouter = APIRouter()
 
 # For Development only
 @usersRouter.get("/")
 def getUsers():
-    return {}
+    cursor.execute("SELECT * FROM Users;")
+    return cursor.fetchall()
 
 @usersRouter.post("/")
 def createUser(user: CreateUser):
