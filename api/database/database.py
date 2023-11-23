f47bfbd24397f2db2253b8d46afb3888b1c15837
Daniel McPherson <daniel.mcpherson@live.co.uk>
2023-11-23 20:19:25 +0000

    Added dictionary=True to the cursor

--- api/database/database.py
+++ api/database/database.py
@@ -1,8 +1,6 @@
 import mysql.connector
-import MySQLdb.cursors.DictCursor
 
 connection = mysql.connector.connect(user="link", password="password",
                               host="linkshortener-db-1",
-                              database="LinkShortener",
-                              cursorclass=MySQLdb.cursors.DictCursor)
-cursor = connection.cursor()
+                              database="LinkShortener")
+cursor = connection.cursor(dictionary=True)
