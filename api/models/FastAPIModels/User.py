eac3157425bad34b5c8cfc94c17c424a2c901ed9
Daniel McPherson <daniel.mcpherson@live.co.uk>
2023-11-23 18:32:10 +0000

    Moved into FastAPIModels

---
+++ api/models/FastAPIModels/User.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel, EmailStr
+
+class User(BaseModel):
+    UserID: int
+    FirstName: str
+    LastName: str
+    Email: EmailStr
+    Password: str
+    SessionToken: str
+
+class CreateUser(BaseModel):
+    FirstName: str
+    LastName: str
+    Email: EmailStr
+    Password: str
