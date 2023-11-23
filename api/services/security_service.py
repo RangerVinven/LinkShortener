660b608202ef100092d6b5bbc5bbcdbd73612ee0
Daniel McPherson <daniel.mcpherson@live.co.uk>
2023-11-23 22:18:23 +0000

    added hashPassword function

--- api/services/security_service.py
+++ api/services/security_service.py
@@ -1,4 +1,8 @@
 from secrets import token_hex
+import hashlib
 
 def generateSessionToken():
     return token_hex(16)
+
+def hashPassword(password: str):
+    return hashlib.sha256(password).hexdigest()
