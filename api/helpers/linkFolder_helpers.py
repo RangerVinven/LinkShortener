367a3117165f40121f987acebe6f2c51a569e740
Daniel McPherson <daniel.mcpherson@live.co.uk>
2023-11-27 23:07:31 +0000

    Created doesUserOwnLinkFolder

--- api/helpers/linkFolder_helpers.py
+++ api/helpers/linkFolder_helpers.py
@@ -9,3 +9,15 @@ async def getUserIDFromRequest(request: Request):
     userID = cursor.fetchone()["UserID"]
 
     return userID
+
+# Checks whether the user can access the LinkFolder they want to modify
+async def doesUserOwnLinkFolder(linkFolderID: int, userID: int):
+    # Makes sure the LinkFolder is owned by the user
+    # Below query should return a list with the FolderID if the LinkFolder is owned by the user
+    cursor.execute("SELECT FolderID FROM LinkFolders WHERE FolderID=%s AND UserID=%s", (linkFolderID, userID))
+    queryResponse = cursor.fetchall() # Should be a list with 1 element if it's owned by the user
+
+    if len(queryResponse) != 1:
+        return False
+
+    return True
