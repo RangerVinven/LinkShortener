8f37ed513ec6b41d1f6c1bc816fb3b47d85ab8ff
Daniel McPherson <daniel.mcpherson@live.co.uk>
2023-11-27 21:20:48 +0000

    Added create link folder endpoint

--- api/routes/link_folders.py
+++ api/routes/link_folders.py
@@ -1,10 +1,15 @@
 
 from fastapi import APIRouter, Request
 
-from services.linkFolder_service import get_linkFolders
+from models.LinkFolder import CreateLinkFolder
+from services.linkFolder_service import get_linkFolders, create_linkFolders
 
 linkFoldersRouter = APIRouter()
 
 @linkFoldersRouter.get("/")
 async def getLinkFolders(request: Request):
     return await get_linkFolders(request)
+
+@linkFoldersRouter.post("/")
+async def createLinkFolder(linkFolder: CreateLinkFolder, request: Request):
+    return await create_linkFolders(linkFolder, request)
