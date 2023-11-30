43ef6f746c6530a76e00b5ebecbdeb00e527a1df
Daniel McPherson <daniel.mcpherson@live.co.uk>
2023-11-30 17:26:50 +0000

    Created get_links function

---
+++ api/services/links_service.py
@@ -0,0 +1,12 @@
+from fastapi import Request
+
+from services.database_service import cursor
+
+async def get_links(request: Request):
+    # Gets the user's sesison token
+    sessionToken = request.cookies.get("SessionToken")
+
+    cursor.execute("SELECT l.LinkCode, l.LinkName, l.RedirectsTo, l.NumberOfVisits, l.IsEnabled, l.IsExpired, l.StartDate, l.ExpiryDate, l.DateCreated, l.FolderID FROM Links AS l INNER JOIN LinkFolders as lk ON l.FolderID=lk.FolderID INNER JOIN Users as u ON lk.UserID=u.UserID WHERE u.SessionToken=%s", (sessionToken,))
+    links = cursor.fetchall()
+
+    return links
