0c70cca2cc3148be34dd7e402549e41cedc3d400
Daniel McPherson <daniel.mcpherson@live.co.uk>
2023-11-30 17:27:07 +0000

    Added /links router

--- api/main.py
+++ api/main.py
@@ -2,11 +2,13 @@ from fastapi import FastAPI
 
 from routes.users import usersRouter
 from routes.link_folders import linkFoldersRouter
+from routes.links import linksRouter
 
 api = FastAPI()
 
 api.include_router(usersRouter, prefix="/users")
 api.include_router(linkFoldersRouter, prefix="/linkfolders")
+api.include_router(linksRouter, prefix="/links")
 
 @api.get("/")
 def root():
