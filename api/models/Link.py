18d24e123470586da3c5e50de51539d9f6b1f1c7
Daniel McPherson <daniel.mcpherson@live.co.uk>
2023-11-30 18:11:41 +0000

    Added CreateLink model

--- api/models/Link.py
+++ api/models/Link.py
@@ -13,3 +13,13 @@ class Link(BaseModel):
     ExpiryDate: date | None = None
     DateCreated: date
     FolderID: int
+
+class CreateLink(BaseModel):
+    LinkName: str
+    RedirectsTo: str
+    NumberOfVisits: int
+    IsEnabled: bool
+    IsExpired: bool
+    StartDate: date | None = None
+    ExpiryDate: date | None = None
+    DateCreated: date
